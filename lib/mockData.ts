965a26a6c52b48d25ff15cecf17f0fcb138c4e50
baldat20 <132357870+baldat20@users.noreply.github.com>
2026-01-12 22:18:52 -0300

    Create mockData.ts

---
+++ lib/mockData.ts
@@ -0,0 +1,26 @@
+export const dadosMock = [
+  {
+    supervisor: "FABIO",
+    tecnico: "BRUNO ALEX DA SILVA",
+    agendado: 2,
+    chegada: 0,
+    concluida: 3,
+    despachado: 0,
+    deslocamento: 0,
+    execucao: 1,
+    total: 6,
+    meta: 7
+  },
+  {
+    supervisor: "TIAGO",
+    tecnico: "LUCAS FELIX DA SILVA",
+    agendado: 3,
+    chegada: 1,
+    concluida: 4,
+    despachado: 0,
+    deslocamento: 1,
+    execucao: 0,
+    total: 9,
+    meta: 8
+  }
+]
