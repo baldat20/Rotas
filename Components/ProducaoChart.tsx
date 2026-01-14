756f5bd4234bb490bce0f84b88b85875e8609a40
baldat20 <132357870+baldat20@users.noreply.github.com>
2026-01-13 21:17:10 -0300

    Update ProducaoChart.tsx

--- Components/ProducaoChart.tsx
+++ Components/ProducaoChart.tsx
@@ -1,19 +1,31 @@
 import {
-  BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Line
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Line
 } from "recharts"
 
 export function ProducaoChart({ data }) {
   return (
     <div className="bg-white p-4 rounded-xl shadow h-96">
-      <h2 className="font-semibold mb-2">Produção x Meta por Técnico</h2>
+      <h2 className="font-semibold mb-2">
+        Produção × Meta por Técnico
+      </h2>
 
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data}>
-          <XAxis dataKey="tecnico" hide />
+          <XAxis dataKey="Nome do Técnico" hide />
           <YAxis />
           <Tooltip />
-          <Bar dataKey="total" />
-          <Line type="monotone" dataKey="meta" />
+          <Bar dataKey="Total geral" />
+          <Line
+            type="monotone"
+            dataKey="Meta"
+            strokeWidth={3}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
